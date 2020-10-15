e5db2fd0b010bbe0ad252830bc43863c8ca52642
n-nitta <n-nitta@konan-u.ac.jp>
2020-10-15 11:16:26 +0900

    Initial commit

---
+++ app/src/test/java/com/example/githubexerciseforandroidstudio/Main.java
@@ -0,0 +1,10 @@
+package com.example.githubexerciseforandroidstudio;
+
+import org.junit.Test;
+
+public class Main {
+    @Test
+    public void test() {
+        System.out.println("Hello Android!!");
+    }
+}
